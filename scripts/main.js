911a76e1a2e7d0bc441080920dc475c2d09e919a
Mike Koss <mckoss@startpad.org>
2011-01-16 15:57:09 -0800

    Auto-update html.

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,52 @@
 // Wiki - A simple wiki base on Pageforest.
+/*globals Showdown */
 namespace.lookup('com.pageforest.wiki').defineOnce(function(ns) {
     var dom = namespace.lookup('org.startpad.dom');
     var client = new namespace.com.pageforest.client.Client(ns);
     var markdown = new Showdown.converter();
-    
+
     var parts;
+    var lastMarkdown = "";
+    var syncTime = 5;
     
     function onEdit(evt) {
         $(parts['section-edit']).toggle('3s');
     }
 
+    function onEditChange() {
+        if (parts['section-edit'].value == lastMarkdown) {
+            return;
+        }
+        lastMarkdown = parts['section-edit'].value;
+        parts.section.innerHTML = markdown.makeHtml(lastMarkdown);
+    }
+    
     function onReady() {
         parts = dom.bindIDs();
         client.addAppBar();
 
         $(parts.edit).click(onEdit);
+        $(parts['section-edit']).keydown(onEditChange);
+        
+        setInterval(onEditChange, syncTime * 1000);
     }
 
-    function setDoc(json) {
-        $('#section').html(markdown.makeHtml(json.blob.markdown));
-        $('#section-edit').val(json.blob.markdown);
-        updateMeta(json);
-    }
-    
     function updateMeta(json) {
         document.title = json.title;
         $('#title').text(json.title);
     }
 
+    function setDoc(json) {
+        parts['section-edit'].value = json.blob.markdown;
+        onEditChange();
+        updateMeta(json);
+    }
+    
     function getDoc() {
         return {
             blob: {
                 version: 1,
-                markdown: $('#section-edit').val()
+                markdown: parts['section-edit'].value
             },
             readers: ['public']
         };
